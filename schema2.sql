a9f9a00f0536e01190a0f75d8d5b8cc9fd9fcc56
Marcos-111 <escalzomd@gmail.com>
2020-09-08 03:11:28 +0200

    Schema

---
+++ schema2.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS autor;
+
+DROP TABLE IF EXISTS libro;
+
+CREATE TABLE autor(
+    [id] INTEGER PRIMARY KEY,
+    [author] STRING NOT NULL
+);
+
+CREATE TABLE libro(
+    [id] INTEGER PRIMARY KEY AUTOINCREMENT,
+    [title] STRING NOT NULL,
+    [pags] INTEGER NOT NULL,
+    [fk_author_id] INTEGER NOT NULL REFERENCES autor(id)
+);
+
+    
